5ddf3950f52a71651aa0a189c32ca588f580f460
Mikail Stolpovskikh <monkeysbrain@yandex.ru>
2026-09-01 22:15:28 +0300

    fix v19 wrapper around v18

--- tools/live_mono_imu_state_chain_v19.cpp
+++ tools/live_mono_imu_state_chain_v19.cpp
@@ -1,12 +1,12 @@
 #include <gflags/gflags.h>
 DECLARE_bool(no_incremental_pose);
 
-#define main jtzero_state_chain_v18_main
+#define JTZERO_V18_NO_MAIN
 #include "live_mono_imu_state_chain_v18.cpp"
-#undef main
+#undef JTZERO_V18_NO_MAIN
 
 int main(int argc, char** argv) {
   FLAGS_no_incremental_pose = true;
   std::cout << "[v19] optimizer state output enabled\n";
-  return jtzero_state_chain_v18_main(argc, argv);
+  return runStateChainV18(argc, argv);
 }

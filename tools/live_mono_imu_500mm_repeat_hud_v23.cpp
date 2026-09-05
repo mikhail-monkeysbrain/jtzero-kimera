15bcf24e5b2a6738d319145e4a1dcbc58721fd7c
Mikail Stolpovskikh <monkeysbrain@yandex.ru>
2026-09-05 17:00:02 +0300

    Enable strict stall rejection in V23

--- tools/live_mono_imu_500mm_repeat_hud_v23.cpp
+++ tools/live_mono_imu_500mm_repeat_hud_v23.cpp
@@ -6,6 +6,8 @@
 #include "v18_parts/live_mono_imu_500mm_repeat_hud_v18_part06a.inc"
 #include "v18_parts/live_mono_imu_500mm_repeat_hud_v18_part06b.inc"
 #include "v18_parts/live_mono_imu_500mm_repeat_hud_v18_part07a.inc"
+#define JTZERO_STRICT_STALL_NS 500000000LL
 #include "v18_parts/live_mono_imu_500mm_repeat_hud_v18_part07b.inc"
+#undef JTZERO_STRICT_STALL_NS
 #include "v23_parts/live_mono_imu_500mm_repeat_hud_v23_part08a.inc"
 #include "v23_parts/live_mono_imu_500mm_repeat_hud_v23_part08b.inc"

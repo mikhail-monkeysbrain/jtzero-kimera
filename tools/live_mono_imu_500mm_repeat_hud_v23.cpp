cf9b3a2fa51bcfa8615349d661c780954a7ec1ca
Mikail Stolpovskikh <monkeysbrain@yandex.ru>
2026-09-05 17:00:48 +0300

    Enable balanced direction HUD in V23

--- tools/live_mono_imu_500mm_repeat_hud_v23.cpp
+++ tools/live_mono_imu_500mm_repeat_hud_v23.cpp
@@ -4,8 +4,10 @@
 #include "v18_parts/live_mono_imu_500mm_repeat_hud_v18_part04.inc"
 #include "v18_parts/live_mono_imu_500mm_repeat_hud_v18_part05.inc"
 #include "v18_parts/live_mono_imu_500mm_repeat_hud_v18_part06a.inc"
+#define JTZERO_B_FIRST_HUD
 #include "v18_parts/live_mono_imu_500mm_repeat_hud_v18_part06b.inc"
 #include "v18_parts/live_mono_imu_500mm_repeat_hud_v18_part07a.inc"
+#undef JTZERO_B_FIRST_HUD
 #define JTZERO_STRICT_STALL_NS 500000000LL
 #include "v18_parts/live_mono_imu_500mm_repeat_hud_v18_part07b.inc"
 #undef JTZERO_STRICT_STALL_NS

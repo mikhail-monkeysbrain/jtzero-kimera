7e98bc8f040631312d965e02c880878953934de3
Mikail Stolpovskikh <monkeysbrain@yandex.ru>
2026-09-09 11:25:47 +0300

    diag: add V43 camera forensic target

---
+++ tools/live_mono_imu_500mm_v43_camera_forensic.cpp
@@ -0,0 +1,6 @@
+#define JTZERO_V41_METRIC_CAMERA
+#define JTZERO_V42_DIRECT_LUNA
+#define JTZERO_V43_CAMERA_FORENSIC
+#define JTZERO_LEG_COUNT 1
+#define JTZERO_SINGLE_LEG_MODE
+#include "live_mono_imu_500mm_repeat_hud_v25.cpp"
